966a4ecf4cf2b063bc8c0f570eb0aac7a56aa109
Isaac <govegeku00@gmail.com>
2023-10-25 21:44:34 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
 
 function NavBar() {
   return (
@@ -8,23 +10,21 @@ function NavBar() {
   );
 }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
-{/* write an <About> component here */}
+/* write an <About> component here */
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* add the <Home> component here */
+      <Home />
+      }
+      {/* add your <About> component here */
+      <About />}
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+     <nav>
+      <a href="#home">I'm a link!</a>
+     </nav>
+    </div>
+  )
+}
+
+export default Navbar
